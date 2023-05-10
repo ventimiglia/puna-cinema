b7e878085a18b86ff739c248f372afecc51247c5
ventimiglia <sebaventimiglia23@gmail.com>
2023-05-10 17:00:22 -0300

    remove metadata export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import "./globals.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import StyledComponentsRegistry from "./registry";
 import WatchlistProvider from "./(home)/Context/WatchList";
+import Head from "next/head";
 
-export const metadata = {
+const metadata = {
   title: "Puna Cinema",
   description: "The new list of movies",
 };
@@ -21,6 +22,10 @@ export default function RootLayout({
     <WatchlistProvider>
       <QueryClientProvider client={queryClient}>
         <StyledComponentsRegistry>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+          </Head>
           <html lang="en">
             <body>{children}</body>
           </html>
